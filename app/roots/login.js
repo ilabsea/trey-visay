f3ab6e3ceac71faf6ef97fe826c9f4692708911b
Sokly <hengsokly23@gmail.com>
2017-10-03 10:50:50 +0700

    sign in: add validation

--- app/roots/login.js
+++ app/roots/login.js
@@ -5,7 +5,10 @@ import {
   ScrollView,
   TextInput,
   StyleSheet,
-  Button
+  Button,
+  KeyboardAvoidingView,
+  Alert,
+  TouchableOpacity,
 } from 'react-native';
 
 import realm from '../schema';
@@ -20,7 +23,7 @@ class Login extends Component {
 
   constructor(props) {
     super(props);
-    this.state = { userName: '' };
+    this.state = { userName: '', password: '' };
     this.handleUsernameChange = this.handleUsernameChange.bind(this);
     this.handlePasswordChange = this.handlePasswordChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
@@ -39,16 +42,21 @@ class Login extends Component {
   }
 
   handleSubmit(event) {
-    if (!this.state.userName || !this.state.password) {
-      alert('please filling the user name and password');
-      return;
-    }
-
     let users = realm.objects('User').filtered('userName="' + this.state.userName + '" AND password="' + this.state.password + '"');
     if (!!users.length) {
       this.props.navigation.navigate('ProfileForm');
+      return;
+    }
+
+    let usernames = realm.objects('User').filtered('userName="' + this.state.userName + '"');
+    if (!!usernames.length) {
+      Alert.alert(
+        'Incorrect password',
+        'The passwrod you entered is incorrect. Please try atain.')
     } else {
-      alert('your username and password is invalid');
+      Alert.alert(
+        'Incorrect username',
+        "The username you entered doesn't appear to belong to an account. Please check your username and try again.")
     }
 
     // if (this.state.userName == 'sokly' && this.state.password == '123456') {
@@ -60,74 +68,97 @@ class Login extends Component {
 
   render() {
     const { navigate } = this.props.navigation;
+    const isEnabled = this.state.userName.length > 0 && this.state.password.length > 0;
 
     return (
-      <View style={styles.container}>
-        <View style={{marginTop: 16, marginBottom: 16}}>
-          <Text style={styles.subTitle}>Sign In</Text>
-          <Text>to continue to Trey Visay</Text>
+      <KeyboardAvoidingView behavior='padding' style={styles.container}>
+        <View
+          style={{flexGrow: 1}}>
+          <View style={{marginTop: 16, marginBottom: 16}}>
+            <Text style={styles.subTitle}>Sign In</Text>
+            <Text style={{color: '#fff'}}>to continue to Trey Visay</Text>
+          </View>
+
+          <TextInput
+            style={styles.inputText}
+            onChangeText={this.handleUsernameChange}
+            returnKeyType='next'
+            placeholder='Username'
+            placeholderTextColor='rgba(255,255,255,0.7)'
+            autoCorrect={false}
+            value={this.state.username}
+            underlineColorAndroid='rgba(0,0,0,0)'
+            onSubmitEditing={() => this.passwordInput.focus()}
+          />
+
+          <TextInput
+            style={styles.inputText}
+            secureTextEntry={true}
+            returnKeyType='go'
+            placeholder='Password'
+            placeholderTextColor='rgba(255,255,255,0.7)'
+            onChangeText={this.handlePasswordChange}
+            value={this.state.password}
+            ref={(input) => this.passwordInput = input}
+          />
+
+          <Button
+            onPress={this.handleSubmit}
+            title="Login"
+            accessibilityLabel="Next to continue login"
+            disabled={!isEnabled}
+          />
         </View>
 
-        <Text style={styles.inputLabel}>Name</Text>
-        <TextInput
-          style={styles.inputText}
-          onChangeText={this.handleUsernameChange}
-          value={this.state.userName}
-        />
+        <View style={styles.createAccountContainer}>
+          <TouchableOpacity
+            style={styles.createAccount}
+            onPress={() => navigate('Register')}>
+            <Text style={{fontSize: 16, color: '#fff'}}>Create New Trey Visay Account</Text>
+          </TouchableOpacity>
+        </View>
 
-        <Text style={styles.inputLabel}>Password</Text>
-        <TextInput
-          style={styles.inputText}
-          secureTextEntry={true}
-          onChangeText={this.handlePasswordChange}
-        />
-
-        <Button
-          style={styles.createAccount}
-          onPress={this.handleSubmit}
-          title="Submit"
-          accessibilityLabel="Next to continue login"
-        />
-
-        <Button
-          style={styles.createAccount}
-          onPress={() => navigate('Register')}
-          title="Create New Trey Visay Account"
-          accessibilityLabel="Next to continue login"
-        />
-
-      </View>
+      </KeyboardAvoidingView>
     )
   }
 }
 
 const styles = StyleSheet.create({
   signIn: {
-    marginTop: 40
+    marginTop: 40,
+    color: '#fff',
+  },
+  createAccountContainer: {
+    padding: 24
   },
   createAccount: {
-    alignSelf: 'flex-end',
-    backgroundColor: 'transparent'
+    alignSelf: 'center',
+    borderWidth: 0.5,
+    borderColor: '#fff',
+    paddingHorizontal: 15,
+    paddingVertical: 5
   },
   container: {
     padding: 24,
     flex: 1,
-    flexDirection: 'column'
+    backgroundColor: '#1abc9c',
   },
   titleText: {
     fontSize: 20,
     fontWeight: 'bold'
   },
   subTitle: {
-    fontSize: 16
+    fontSize: 16,
+    color: '#fff'
   },
   inputText: {
     height: 40,
-    paddingLeft: 5,
-    paddingRight: 5
+    backgroundColor: 'rgba(255,255,255,0.2)',
+    paddingHorizontal: 10,
+    marginBottom: 20,
+    color: '#fff',
   },
   inputLabel: {
-
   }
 })
 
