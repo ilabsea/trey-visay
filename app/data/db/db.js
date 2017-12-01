ca5b5fb2e30e5da406f40dca5176627c56455acd
Sokly <hengsokly23@gmail.com>
2017-12-01 14:23:03 +0700

    remove unused code

--- app/data/db/db.js
+++
@@ -1,9 +0,0 @@
-class Db {
-  query(model: string, filter?: string) {
-    let results = this.realm.objects(model);
-    if(filter) {
-        return results.filtered(filter);
-    }
-    return results;
-  }
-}
