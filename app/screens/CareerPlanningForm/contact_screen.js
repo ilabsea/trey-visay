328f5ecdb656445343381c00dc88786c9a3d7ce0
Sokly <hengsokly23@gmail.com>
2017-12-18 16:08:27 +0700

    click on school go to school detail

--- app/screens/CareerPlanningForm/contact_screen.js
+++ app/screens/CareerPlanningForm/contact_screen.js
@@ -23,6 +23,7 @@ import shareStyles from './style';
 
 import realm from '../../schema';
 import User from '../../utils/user';
+import schoolList from '../../data/json/schools';
 
 export default class ContactScreen extends Component {
   static navigationOptions = ({ navigation }) => {
@@ -60,7 +61,7 @@ export default class ContactScreen extends Component {
     return(
       <View style={shareStyles.footerWrapper}>
         <TouchableOpacity onPress={ this._goNext.bind(this) } style={shareStyles.btnNext}>
-          <Text style={shareStyles.btnText}>បន្តទៀត</Text>
+          <Text style={shareStyles.btnText}>បញ្ចប់</Text>
           <Icon name='keyboard-arrow-right' color='#fff' size={24} />
         </TouchableOpacity>
       </View>
@@ -88,46 +89,60 @@ export default class ContactScreen extends Component {
   }
 
   _renderContent() {
-    let schools = [
-      { name: 'សកលវិទ្យាល័យ បញ្ញាសាស្រ្តកម្ពុជា', logo: require('../../assets/images/schools/1.png'), address: 'ផ្លូវលេខ ១៨៤, មហាវិថី ព្រះនរត្តម ក្រុងភ្នំពេញ', phoneNumber: '087 777 970', website: 'www.puc.edu.kh' },
-      { name: 'សកលវិទ្យាល័យកម្ពុជា', logo: require('../../assets/images/schools/2.png'), address: 'វិថីណ័រប្រីត (១០១៩) សង្កាត់ទឹកថ្លា ខ័ណ្ឌសែនសុខ រាជធានីភ្នំពេញ', phoneNumber: '023 993 276 / 060 3333 46 / 093 777 576 / 069 521 111', website: 'www.uc.edu.kh' },
-      { name: 'សកលវិទ្យាល័យវិទ្យាសាស្រ្តសុខាភិបាល', logo: require('../../assets/images/schools/3.png'), address: 'ផ្លូវលេខ ១៨៤, មហាវិថី ព្រះនរត្តម ក្រុងភ្នំពេញ', phoneNumber: '093 278 899', website: 'www.uhs.edu.kh' },
-      { name: 'សាកលវិទ្យាល័យចំរើនពហុបច្ចេកវិទ្យា', logo: require('../../assets/images/schools/4.png'), address: 'អគា១៥៤, ផ្លូវ១៣២, សង្កាត់ ទឹកល្អក់១ ខ័ណ្ឌទួលគោក រាជធានីភ្នំពេញ', phoneNumber: '061 800 023', website: 'www.cup.edu.kh' },
-      { name: 'សាកលវិទ្យាល័យសេដ្ឋកិច្ចនិងហិរញ្ញវត្ថុ', logo: require('../../assets/images/schools/5.png'), address: 'អគា៦៨, ផ្លូវ ៥២៨,២៩១, សង្កាត់ បឹងកក់១, ខ័ណ្ឌទួលគោក, ភ្នំពេញ', phoneNumber: '010 516 887', website: 'www.uef.edu.kh' },
-    ]
+    // let schools = [
+    //   { name: 'សកលវិទ្យាល័យ បញ្ញាសាស្រ្តកម្ពុជា', logo: require('../../assets/images/schools/1.png'), address: 'ផ្លូវលេខ ១៨៤, មហាវិថី ព្រះនរត្តម ក្រុងភ្នំពេញ', phoneNumber: '087 777 970', website: 'www.puc.edu.kh' },
+    //   { name: 'សកលវិទ្យាល័យកម្ពុជា', logo: require('../../assets/images/schools/2.png'), address: 'វិថីណ័រប្រីត (១០១៩) សង្កាត់ទឹកថ្លា ខ័ណ្ឌសែនសុខ រាជធានីភ្នំពេញ', phoneNumber: '023 993 276 / 060 3333 46 / 093 777 576 / 069 521 111', website: 'www.uc.edu.kh' },
+    //   { name: 'សកលវិទ្យាល័យវិទ្យាសាស្រ្តសុខាភិបាល', logo: require('../../assets/images/schools/3.png'), address: 'ផ្លូវលេខ ១៨៤, មហាវិថី ព្រះនរត្តម ក្រុងភ្នំពេញ', phoneNumber: '093 278 899', website: 'www.uhs.edu.kh' },
+    //   { name: 'សាកលវិទ្យាល័យចំរើនពហុបច្ចេកវិទ្យា', logo: require('../../assets/images/schools/4.png'), address: 'អគា១៥៤, ផ្លូវ១៣២, សង្កាត់ ទឹកល្អក់១ ខ័ណ្ឌទួលគោក រាជធានីភ្នំពេញ', phoneNumber: '061 800 023', website: 'www.cup.edu.kh' },
+    //   { name: 'សាកលវិទ្យាល័យសេដ្ឋកិច្ចនិងហិរញ្ញវត្ថុ', logo: require('../../assets/images/schools/5.png'), address: 'អគា៦៨, ផ្លូវ ៥២៨,២៩១, សង្កាត់ បឹងកក់១, ខ័ណ្ឌទួលគោក, ភ្នំពេញ', phoneNumber: '010 516 887', website: 'www.uef.edu.kh' },
+    // ]
+    let schools = schoolList.slice(0, 3);
 
     return (
       <View style={{marginTop: 20}}>
         <Text>ពត៌មានសាលា និង ទំនាក់ទំនង</Text>
         { schools.map((school, i) => {
-          return (
-            <View style={[styles.box, {flexDirection: 'row'}]} key={i}>
-              <View>
-                <Image source={school.logo} style={{width: 100, height: 100}} />
-              </View>
-
-              <View style={{flex: 1, marginLeft: 16}}>
-                <Text style={styles.subTitle}>{school.name}</Text>
-
-                <View style={{flexDirection: 'row'}}>
-                  <AwesomeIcon name='map-marker' color='#1976d2' size={24} />
-                  <Text style={{marginLeft: 8}}>{school.address}</Text>
-                </View>
-
-                <View style={{flexDirection: 'row'}}>
-                  <AwesomeIcon name='phone' color='#1976d2' size={24} />
-                  <Text style={{marginLeft: 8}}>{school.phoneNumber}</Text>
-                </View>
-
-                <View style={{flexDirection: 'row'}}>
-                  <AwesomeIcon name='globe' color='#1976d2' size={24} />
-                  <Text style={{marginLeft: 8}}>{school.website}</Text>
-                </View>
-              </View>
-            </View>
-          )
+          { return(this._renderSchool(school, i)) }
         })}
       </View>
+    )
+  }
+
+  _renderSchool(school, i) {
+    let logo = require('../../assets/images/schools/default.png');
+    if (school.logo) {
+      logo = { uri: school.logo };
+    }
+
+    return (
+      <TouchableOpacity
+        style={[styles.box, {flexDirection: 'row'}]}
+        onPress={() => {this.props.navigation.navigate('InstitutionDetail', {id: school.id})}}
+        key={i}>
+
+        <View>
+          <Image source={logo} style={{width: 100, height: 100}} />
+        </View>
+
+        <View style={{flex: 1, marginLeft: 16}}>
+          <Text style={styles.subTitle}>{school.universityName}</Text>
+
+          <View style={{flexDirection: 'row'}}>
+            <AwesomeIcon name='map-marker' color='#1976d2' size={24} />
+            <Text style={{marginLeft: 8}}>{school.address}</Text>
+          </View>
+
+          <View style={{flexDirection: 'row'}}>
+            <AwesomeIcon name='phone' color='#1976d2' size={24} />
+            <Text style={{marginLeft: 8}}>{school.phoneNumbers.join('; ')}</Text>
+          </View>
+
+          <View style={{flexDirection: 'row'}}>
+            <AwesomeIcon name='globe' color='#1976d2' size={24} />
+            <Text style={{marginLeft: 8}}>{school.websiteOrFacebook.join('; ')}</Text>
+          </View>
+        </View>
+      </TouchableOpacity>
     )
   }
 
