015963b05dfb3ab04808f245f7e31676b0183593
Sokly <hengsokly23@gmail.com>
2017-12-27 16:44:43 +0700

    add personality report screen

--- app/screens/CareerPlanningForm/game_history/personality_report.js
+++ app/screens/CareerPlanningForm/game_history/personality_report.js
@@ -2,21 +2,21 @@ import React, {Component} from 'react';
 import {
   Text,
   View,
-  Button,
   ScrollView,
-  StyleSheet,
-  TouchableOpacity,
 } from 'react-native';
 
 import {
   ThemeProvider,
-  Toolbar,
-  Icon,
 } from 'react-native-material-ui';
+
+import AwesomeIcon from 'react-native-vector-icons/FontAwesome';
 
 import headerStyles from '../../../assets/style_sheets/header';
 import shareStyles from '../../../assets/style_sheets/profile_form';
 import StatusBar from '../../../components/status_bar';
+import personalityJobs from '../../../data/json/personality_jobs';
+import realm from '../../../schema';
+import User from '../../../utils/user';
 
 export default class PersonalityReport extends Component {
   static navigationOptions = ({ navigation }) => {
@@ -26,33 +26,59 @@ export default class PersonalityReport extends Component {
       title: 'ជ្រើសរើសមុខរបរផ្អែកលើបុគ្គលិកលក្ខណៈ',
       headerTitle: <Text style={headerStyles.headerTitleStyle}>ជ្រើសរើសមុខរបរផ្អែកលើបុគ្គលិកលក្ខណៈ</Text>,
       headerStyle: headerStyles.headerStyle,
-      headerLeft: <ThemeProvider uiTheme={{}}>
-                    <TouchableOpacity onPress={() => goBack()} style={{marginHorizontal: 16}}>
-                      <Icon name='close' color='#fff' size={24} />
-                    </TouchableOpacity>
-                  </ThemeProvider>,
+      headerTintColor: '#fff'
     }
   };
 
+  componentWillMount() {
+    let user = realm.objects('User').filtered('uuid="' + User.getID() + '"')[0];
+    let game = user.games.filtered('uuid=="' + this.props.navigation.state.params.gameUuid + '"')[0];
+    let allCareers = [];
+    let personalityCareers = game.personalityCareers.map(career => career.value);
+
+    for (let i=0; i<personalityJobs.length; i++) {
+      allCareers = allCareers.concat(personalityJobs[i].careers);
+    }
+
+    let jobs = allCareers.filter(career => personalityCareers.includes(career.id));
+
+    this.state = {
+      user: user,
+      game: game,
+      jobs: jobs,
+    }
+  }
+
+  _renderContent() {
+    return (
+      <View style={shareStyles.box}>
+        <Text style={shareStyles.subTitle}>មុខរបរ</Text>
+
+        { this.state.jobs.map((job, i) => {
+          return (
+            <View key={i} style={{flexDirection: 'row', alignItems: 'center', marginVertical: 8}}>
+              <AwesomeIcon name='check-circle' size={24} color='#4caf50' style={{marginRight: 8}} />
+              <Text style={{fontFamily: 'KantumruyBold'}}>{job.title}</Text>
+            </View>
+          )
+        })}
+      </View>
+    )
+  }
+
   render() {
     return (
-      null
+      <ThemeProvider uiTheme={{}}>
+        <View style={{flex: 1}}>
+          <StatusBar />
+
+          <ScrollView style={{flex: 1}}>
+            <View style={{margin: 16, flex: 1}}>
+              { this._renderContent() }
+            </View>
+          </ScrollView>
+        </View>
+      </ThemeProvider>
     )
   }
 }
-
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1
-  },
-  scrollContainer: {
-    padding: 16
-  },
-  paragraph: {
-    marginBottom: 16
-  },
-  textBold: {
-    fontFamily: 'KantumruyBold'
-  }
-});
