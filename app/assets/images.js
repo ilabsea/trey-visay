37f26c56628a8245310895968a78ab2f157cae2e
Sokly <hengsokly23@gmail.com>
2018-01-22 10:32:20 +0700

    remove unused icon

--- app/assets/images.js
+++ app/assets/images.js
@@ -2,27 +2,9 @@
 
 const images = {
   // career icons
-  agriculture_and_natural_resource: require('./images/careers/agriculture_and_natural_resource.png'),
-  architect_andengineer: require('./images/careers/architect_and_engineer.png'),
-  art_and_communication: require('./images/careers/art_and_communication.png'),
-  business_and_admin: require('./images/careers/business_and_admin.png'),
-  education_and_training: require('./images/careers/education_and_training.png'),
-  finance_and_insurance: require('./images/careers/finance_and_insurance.png'),
-  admin_and_public_admin: require('./images/careers/admin_and_public_admin.png'),
-  health_science: require('./images/careers/health_science.png'),
-  hospitality_and_tourism: require('./images/careers/hospitality_and_tourism.png'),
-  service_for_public: require('./images/careers/service_for_public.png'),
-  information: require('./images/careers/information.png'),
-  legal_and_public_safty: require('./images/careers/legal_and_public_safty.png'),
-  manufacturing: require('./images/careers/manufacturing.png'),
-  sale_and_service: require('./images/careers/sale_and_service.png'),
-  science_research_engineering_and_mathematics: require('./images/careers/science_research_engineering_and_mathematics.png'),
-  logistic: require('./images/careers/logistic.png'),
-
-  // value icons
-  artistic_or_creativity: require('./images/values/artistic_or_creativity.png'),
-  logical_or_factual: require('./images/values/logical_or_factual.png'),
-  social_or_relationship: require('./images/values/social_or_relationship.png'),
+  science: require('./images/careers/science.png'),
+  technical: require('./images/careers/technical.png'),
+  social: require('./images/careers/social.png'),
 
   // personality icons
   realistic: require('./images/personalities/realistic.png'),
