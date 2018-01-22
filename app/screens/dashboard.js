6a1e1f9803df773ee82a2393972e69f2d330ba72
Sokly <hengsokly23@gmail.com>
2018-01-22 10:58:55 +0700

    enhance on dashboad pg

--- app/screens/dashboard.js
+++ app/screens/dashboard.js
@@ -48,32 +48,6 @@ export default class Dashboard extends Component {
     };
   }
 
-  _renderButton(options) {
-    return (
-      <TouchableOpacity
-        onPress={() => this.props.navigation.navigate(options.url)}
-        style={[styles.btnBox]}>
-        <View style={[styles.btnFab, {backgroundColor: options.icon_bg_color}]}>
-          <AwesomeIcon name={options.icon_name} size={30} color='#fff' />
-        </View>
-        <Text style={styles.btnLabel}>{options.title}</Text>
-      </TouchableOpacity>
-    )
-  }
-
-  _renderButtonWithMaterialIcon(options) {
-    return (
-      <TouchableOpacity
-        onPress={() => this.props.navigation.navigate(options.url)}
-        style={[styles.btnBox]}>
-        <View style={[styles.btnFab, {backgroundColor: options.icon_bg_color}]}>
-          <MaterialIcon name={options.icon_name} size={36} color='#fff' />
-        </View>
-        <Text style={styles.btnLabel}>{options.title}</Text>
-      </TouchableOpacity>
-    )
-  }
-
   _renderTourtip(isVisited) {
     if (isVisited) {
       return (null);
@@ -130,6 +104,26 @@ export default class Dashboard extends Component {
     });
   }
 
+  _renderButton(options) {
+    return (
+      <TouchableOpacity
+        onPress={() => this.props.navigation.navigate(options.url)}
+        style={[styles.btnBox]}>
+        <View style={[styles.btnFab, {backgroundColor: options.icon_bg_color}]}>
+          { !(options.icon_type == 'material') &&
+            <AwesomeIcon name={options.icon_name} size={50} color='#fff' />
+          }
+
+          { options.icon_type == 'material' &&
+            <MaterialIcon name={options.icon_name} size={56} color='#fff' />
+          }
+        </View>
+        <Text style={styles.btnLabel}>{options.title}</Text>
+        <Text style={{marginTop: 16, padding: 16, textAlign: 'center'}}>{options.description}</Text>
+      </TouchableOpacity>
+    )
+  }
+
   render() {
     return (
       <ThemeProvider uiTheme={uiTheme}>
@@ -144,10 +138,15 @@ export default class Dashboard extends Component {
             />
 
             <View style={styles.scrollContainer}>
-              { this._renderButton({ title: 'វាយតម្លៃមុខរបរ និង អាជីព', url: 'CareerCounsellorScreen', icon_bg_color: '#3f51b5', icon_name: 'briefcase' }) }
-              { this._renderButton({ title: 'គ្រឹះស្ថានសិក្សា', url: 'InstitutionStack', icon_bg_color: '#009688', icon_name: 'graduation-cap' }) }
-              { this._renderButton({ title: 'វីដេអូមុខរបរ', url: 'VideoScreen', icon_bg_color: '#f44336', icon_name: 'video-camera' }) }
-              { this._renderButtonWithMaterialIcon({ title: 'ជំនាញវិជ្ជាជីវៈ', url: 'VocationalJobStack', icon_bg_color: '#1aaf5d', icon_name: 'photo-filter' }) }
+              <View style={{flexDirection: 'row'}}>
+                { this._renderButton({ title: 'វាយតម្លៃមុខរបរ', url: 'CareerCounsellorScreen', icon_bg_color: '#3f51b5', icon_name: 'briefcase', description: 'ធ្វើតេស្តមុខរបរ ឬអាជីព ដោយផ្អែកលើបុគ្គលិកលក្ខណៈដើម្បីជ្រើសរើសមុខរបរសាកសមនឹងអ្នក។' }) }
+                { this._renderButton({ title: 'គ្រឹះស្ថានសិក្សា', url: 'InstitutionStack', icon_bg_color: '#009688', icon_name: 'graduation-cap', description: 'អ្នកអាចមើលពត៌មានសាលា លេខទំនាក់ទំនង និង មុខវិជ្ជាដែលអ្នកចង់បន្តការសិក្សាបន្ទាប់ពីបញ្ចប់ថ្នាក់ទី១២។' }) }
+              </View>
+
+              <View style={{flexDirection: 'row'}}>
+                { this._renderButton({ title: 'ជំនាញវិជ្ជាជីវៈ', url: 'VocationalJobStack', icon_bg_color: '#1aaf5d', icon_name: 'photo-filter', icon_type: 'material', description: 'សំរាប់អ្នកគ្មានលទ្ធភាពបន្តការសិក្សាបរិញ្ញាប័ត្រ អ្នកអាចរៀនជំនាញវិជ្ជាជីវះរយៈពេលខ្លី។' }) }
+                { this._renderButton({ title: 'វីដេអូមុខរបរ', url: 'VideoScreen', icon_bg_color: '#f44336', icon_name: 'video-camera', description: 'យល់ដឹងអំពីមុខរបរ និងអាជីព តាមរយះវីដេអូ។' }) }
+              </View>
             </View>
           </ScrollView>
 
@@ -161,27 +160,26 @@ export default class Dashboard extends Component {
 
 const styles = StyleSheet.create({
   scrollContainer: {
-    padding: 16
+    padding: 8
   },
   btnBox: {
     flex: 1,
-    height: 108,
+    minHeight: 315,
     alignItems: 'center',
-    marginBottom: 16,
+    margin: 8,
     backgroundColor: '#fff',
-    flexDirection: 'row',
     paddingRight: 16,
   },
   btnLabel: {
     fontFamily: 'KhmerOureang',
     fontSize: 24,
     lineHeight: 40,
-    flex: 1,
+    color: '#1976d2',
   },
   btnFab: {
-    width: 60,
-    height: 60,
-    borderRadius: 30,
+    width: 100,
+    height: 100,
+    borderRadius: 50,
     justifyContent: 'center',
     alignItems: 'center',
     margin: 24
