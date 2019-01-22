57e9be67be25ee21059292e229e6c9039e4a20c6
mouyleng <mouyleng@instedd.org>
2019-01-22 16:10:04 +0700

    add global style and fix dashboard icon

--- index.js
+++ index.js
@@ -1,7 +1,35 @@
 import React, { Component } from 'react';
-import { AppRegistry } from 'react-native';
+import { Platform, AppRegistry } from 'react-native';
+
+import {
+  setCustomView,
+  setCustomTextInput,
+  setCustomText,
+  setCustomImage,
+  setCustomTouchableOpacity
+} from 'react-native-global-props';
 
 import App from './app';
+
+const customTextProps = {
+  style: {
+    fontSize: 16,
+    fontFamily: Platform.OS === 'ios' ? 'HelveticaNeue' : 'Kantumruy',
+    lineHeight: Platform.OS === 'ios' ? 0 : 28
+  }
+};
+
+const customTextInputProps = {
+  style: {
+    fontFamily: Platform.OS === 'ios' ? 'HelveticaNeue' : 'Kantumruy',
+    fontSize: 16,
+    height: 48,
+    lineHeight: 28
+  }
+};
+
+setCustomText(customTextProps);
+setCustomTextInput(customTextInputProps);
 
 export default class TreyVisay extends Component {
   render() {
