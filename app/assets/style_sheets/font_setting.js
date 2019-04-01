a107b24815f5a3118fe58457a162a4b8d7f0abe2
mouyleng <mouyleng@instedd.org>
2019-04-01 16:37:52 +0700

    fix small styles on dashboard screen

--- app/assets/style_sheets/font_setting.js
+++ app/assets/style_sheets/font_setting.js
@@ -5,9 +5,9 @@ export const FontSetting = {
   nav_title: RF(2.8),
   tab_label: RF(2.5),
   medium_title: RF(2.5),
+  dashboard_subtitle: RF(2.3),
   text: RF(2.2),
   title: RF(2.2),
   button_text: RF(2.2),
-  dashboard_subtitle: RF(2),
   sub_title: RF(1.8)
 }
