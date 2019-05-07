7b9785d1efd05d255012f1d0dd5d483186727e2c
Sokly <hengsokly23@gmail.com>
2019-05-07 09:44:03 +0700

    (bug): fix filter vocational career list

--- app/screens/careers/index_screen.js
+++ app/screens/careers/index_screen.js
@@ -3,49 +3,37 @@ import {
   Text,
   ScrollView,
   View,
-  TouchableOpacity,
-  Image,
-  StyleSheet
 } from 'react-native';
-import AwesomeIcon from 'react-native-vector-icons/FontAwesome';
 
 // Utils
 import mainStyles from '../../assets/style_sheets/main/main';
-import { FontSetting } from "../../assets/style_sheets/font_setting";
 import ButtonList from '../../components/list/button_list';
 import StatusBar from '../../components/shared/status_bar';
-import SchoolListView from '../../components/schools/school_list';
 import characteristicList from '../../data/json/characteristic_jobs';
 import mapping from '../../data/json/careers/mapping';
-import schoolList from '../../data/json/universities';
 
 export default class CareerIndexScreen extends Component {
+
   constructor(props){
     super(props);
+
     this.state = {
-      careers: []
+      careers: this.getCareers()
     }
   }
 
-  componentWillMount() {
+  getCareers() {
     let careerClusterCode = this.props.navigation.state.params.code;
+    let codes = mapping.filter(obj => obj.career_cluster_code == careerClusterCode);
+    codes = codes.map(x => x.career_code);
 
-    codes = mapping.filter(obj => { return obj.career_cluster_code == careerClusterCode });
-    careers = [];
-    characteristicList.map(obj => {
-      for (let i = 0 ; i < codes.length; i++) {
-        careerCode = codes[i].career_code;
-        obj.careers.filter(c => {
-          if (c.code == careerCode ){
-            careers.push(c)
-          }
-        })
-      }
-    });
+    let careerList = characteristicList.map(x => x.careers);
+    careerList = [].concat.apply([], careerList);
 
-    this.setState({
-      careers : careers
-    })
+    let allCareers = [...new Set(careerList.map(x => x.code))];
+    allCareers = allCareers.map(code => careerList.find(item => item.code == code));
+
+    return allCareers.filter(x => codes.includes(x.code));
   }
 
   renderCareer(career, i) {
