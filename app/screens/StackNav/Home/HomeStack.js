9b106ca11cbc7f91498bbb5ebb5aab5002ad147f
mouyleng <mouyleng@instedd.org>
2019-05-28 17:15:19 +0700

    add tab bar to home page

--- app/screens/StackNav/Home/HomeStack.js
+++ app/screens/StackNav/Home/HomeStack.js
@@ -1,5 +1,10 @@
 import React from 'react';
-import { createStackNavigator } from 'react-navigation';
+import {
+  createBottomTabNavigator,
+  createStackNavigator
+} from 'react-navigation';
+import AwesomeIcon from 'react-native-vector-icons/FontAwesome';
+import MaterialIcon from 'react-native-vector-icons/MaterialIcons';
 
 // Screens
 import Home from '../../home/home';
@@ -7,12 +12,48 @@ import VocationalStack from '../Vocational/VocationalStack';
 import SchoolStack from '../School/SchoolStack';
 import AccountStack from '../account_stack';
 import CareerCounsellorStack from '../CareerCounsellor/CareerCounsellorStack';
+import ProfileStack from '../profile_stack';
 
 import VideoScreen from '../../Video/VideoScreen';
 
-const HomeStack = createStackNavigator({
+const HomeTab = createBottomTabNavigator({
   Home: {
     screen: Home,
+    navigationOptions: ({navigation}) => ({
+      tabBarLabel: 'ទំព័រដេីម',
+      tabBarIcon: ({ focused, horizontal, tintColor }) => {
+        return <MaterialIcon name='home' size={22} color={tintColor} />;
+      },
+    }),
+  },
+  Profile: {
+    screen: ProfileStack ,
+    header: { visible:false },
+    navigationOptions: {
+      tabBarLabel: 'ប្រវត្តិរូបសង្ខេប',
+      tabBarIcon: ({ focused, horizontal, tintColor }) => {
+        return <AwesomeIcon name='user' size={22} color={tintColor} />;
+      },
+    }
+  },
+  Others: {
+    screen: ({ navigation }) => <SchoolScreen screenProps={{category: 'អង្គការ', navigation: navigation}}/>,
+    navigationOptions: {
+      tabBarLabel: 'ផ្សេងៗ',
+      tabBarIcon: ({ focused, horizontal, tintColor }) => {
+        return <AwesomeIcon name='ellipsis-h' size={24} color={tintColor} />;
+      },
+    }
+  },
+}, {
+  tabBarPosition: 'top',
+  animationEnabled: true,
+});
+
+
+const HomeStack = createStackNavigator({
+  Home: {
+    screen: HomeTab,
     navigationOptions: ({navigation}) => ({
       title: 'ទំព័រដេីម'
     }),
