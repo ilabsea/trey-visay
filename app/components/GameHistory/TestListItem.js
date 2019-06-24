7c9d18f504b6c3f233dccd1692aadc4710b43657
Sokly <hengsokly23@gmail.com>
2019-06-24 09:03:34 +0700

    test list item add km number

--- app/components/GameHistory/TestListItem.js
+++ app/components/GameHistory/TestListItem.js
@@ -5,17 +5,21 @@ import mainStyles from '../../assets/style_sheets/main/main';
 import { Colors } from '../../assets/style_sheets/main/colors';
 import { longDateFormat as dateFormat } from '../../utils/date';
 
+const numbers = {0: '០', 1: '១', 2: '២', 3: '៣', 4: '៤', 5: '៥', 6: '៦', 7: '៧', 8: '៨', 9: '៩'};
+
 const TestListItem = (props) => {
-  return(
+  let kmNumber = props.number.toString().split('').map(x => numbers[x]).join('');
+
+  return (
     <TouchableOpacity
       style={styles.list}
       onPress={props.onPress}>
         <View style={styles.number}>
-          <Text style={{color: '#fff', fontSize: 24}}>{props.number}</Text>
+          <Text style={{color: '#fff', fontSize: 24}}>{kmNumber}</Text>
         </View>
 
         <View style={styles.textWrapper}>
-          <Text style={[mainStyles.title, {lineHeight: 42}]}>តេស្តលើកទី {props.number}</Text>
+          <Text style={[mainStyles.title, {lineHeight: 42}]}>តេស្តលើកទី {kmNumber}</Text>
           <Text style={[mainStyles.subTitle, {flex: 1}]}>ធ្វើនៅ: {dateFormat(props.createdAt)}</Text>
         </View>
     </TouchableOpacity>
