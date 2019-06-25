a794cb682ae57cc7b6de9371271222d1ed6b6378
Sokly <hengsokly23@gmail.com>
2019-06-25 15:40:59 +0700

    increase fontsize for home title

--- app/screens/StackNav/Home/HomeStack.js
+++ app/screens/StackNav/Home/HomeStack.js
@@ -3,20 +3,24 @@ import { Platform } from 'react-native';
 import { createStackNavigator } from  'react-navigation';
 
 import Home from '../../home/home';
+import { FontSetting } from '../../../assets/style_sheets/font_setting';
 
 const HomeStack = createStackNavigator(
   {
     HomeStack: {
       screen: Home,
       navigationOptions: ({navigation}) => ({
-        title: 'ទំព័រដេីម'
+        title: 'ទំព័រដេីម',
+        headerTitleStyle: {
+          fontSize: FontSetting.nav_title
+        }
       })
     }
   },
   {
     navigationOptions: ({
       headerStyle: {
-        marginTop: Platform.OS == 'android' ? 24: 0
+        // marginTop: Platform.OS == 'android' ? 24: 0
       },
     }),
   }
