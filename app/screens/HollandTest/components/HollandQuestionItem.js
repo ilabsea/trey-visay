8c68ff937f92e2d2708a78db8d7f6b7459d3a1e5
Sokly <hengsokly23@gmail.com>
2023-05-25 15:27:25 +0700

    Refactor holland questionaire and remove unsed code

---
+++ app/screens/HollandTest/components/HollandQuestionItem.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { View } from 'react-native';
+import { Text, RatingGroup } from '../../../components';
+import { Card } from 'react-native-paper';
+import ratings from '../json/list_ratings';
+
+export default HollandQuestionItem = ({question, index}) => {
+  return (
+    <Card key={index} style={{marginVertical: 8, padding: 8}} >
+      <Text>{index + 1}) {question.name}</Text>
+
+      <View style={{alignItems: 'center'}}>
+        <RatingGroup name={question.code} options={ratings}/>
+      </View>
+    </Card>
+  )
+}
