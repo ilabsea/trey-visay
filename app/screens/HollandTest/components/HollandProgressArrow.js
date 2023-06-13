196ff31f3c0c1dddeb9834307822932d60ae869f
Kimsanlim <kimsanlim27@gmail.com>
2023-06-13 10:35:27 +0700

    Decrease the size of the progress arrow

--- app/screens/HollandTest/components/HollandProgressArrow.js
+++ app/screens/HollandTest/components/HollandProgressArrow.js
@@ -8,7 +8,7 @@ import { FontSetting } from '../../../assets/style_sheets/font_setting';
 
 const HollandProgressArrow = ({step}) => {
   const progressStepStyles = (index) => {
-    let defaultStyles = { width: 50, height: 18, justifyContent: 'center', width: '100%', position: 'relative' }
+    let defaultStyles = { width: '100%', height: 12, justifyContent: 'center', width: '100%', position: 'relative' }
     if (index == 0)
       defaultStyles = {...defaultStyles, borderTopLeftRadius: 5, borderBottomLeftRadius: 5}
     else if (index == 6)
@@ -22,8 +22,8 @@ const HollandProgressArrow = ({step}) => {
 
     const color = index == (step - 2) ? Color.blue : 'white'
     const points = (index == (step - 1) || index == (step - 2)) ? "20,0 28,15 20,30 18,30 18,0" : "20,0 28,15 20,30 18,30 26,15 18,0"
-    return <Svg height="30" width="30" style={{position: 'absolute', right: -18, top: 0}}>
-              <Polygon points={points} fill={color} scale="0.6" />
+    return <Svg height="30" width="30" style={{position: 'absolute', right: -22, top: 0}}>
+              <Polygon points={points} fill={color} scale="0.4" />
             </Svg>
   }
 
@@ -32,13 +32,13 @@ const HollandProgressArrow = ({step}) => {
       return <View key={`arrow-${index}`} style={{zIndex: 7 - index, flex: 1}}>
                 <View style={progressStepStyles(index)}>
                   {renderArrowHead(index)}
-                  <BoldLabelComponent label={index + 1} style={{marginLeft: 24, paddingTop: 1.5, fontSize: 12, color: index + 1 == step ? Color.blue : 'white'}}/>
+                  <BoldLabelComponent label={index + 1} style={{alignSelf: 'center', marginLeft: 2, paddingTop: 0.3, fontSize: 9, color: index + 1 == step ? Color.blue : 'white'}}/>
                 </View>
              </View>
     })
   }
 
-  return <View style={{height: 40, justifyContent: 'center', backgroundColor: Color.blue, paddingHorizontal: 16}}>
+  return <View style={{height: 24, justifyContent: 'flex-start', backgroundColor: Color.blue, paddingHorizontal: 16, borderWidth: 0}}>
             <View style={{flexDirection: 'row', borderWidth: 1, borderColor: 'white', borderRadius: 6}}>
               {renderArrows()}
             </View>
