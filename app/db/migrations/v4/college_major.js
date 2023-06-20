82d48d449686e50c9803867733de21b71dc338aa
Kimsanlim <kimsanlim27@gmail.com>
2023-06-20 17:45:34 +0700

    Create college_major schema and college_majors.json

---
+++ app/db/migrations/v4/college_major.js
@@ -0,0 +1,26 @@
+'use strict';
+
+import Realm from 'realm';
+
+export default class CollegeMajor extends Realm.Object {}
+
+CollegeMajor.schema = {
+  name: 'CollegeMajor',
+  primaryKey: 'uuid',
+  properties: {
+    uuid: 'string',
+    id: 'string?',       // store the id of the college major from the server
+    name: 'string',
+    personality_type: 'string?',
+    general_info: 'string?',
+    orien_orientation_subjects: 'string?',
+    orien_study_condition: 'string?',
+    orien_graduation_condition : 'string?',
+    curriculum : 'string?',
+    teaching_and_learning_process : 'string?',
+    gain_knowledge : 'string?',
+    worthy_career : 'string?',
+    recommendation : 'string?',
+    schools: { type: 'string[]', default: [], optional: true }    // store array of school ID
+  }
+}
--- app/db/schemas/schema4.js
+++ app/db/schemas/schema4.js
@@ -3,13 +3,15 @@ import User from '../migrations/v4/user';
 import Quiz from '../migrations/v4/quiz';
 import School from '../migrations/v4/school';
 import DownloadedImage from '../migrations/v4/downloaded_image';
+import CollegeMajor from '../migrations/v4/college_major';
 
 const schema4 = [
   Sidekiq,
   User,
   Quiz,
   School,
-  DownloadedImage
+  DownloadedImage,
+  CollegeMajor
 ];
 
 export default schema4;
