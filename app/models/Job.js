92f96a28ce9859084d0d13d9f31821c438159f7a
Kimsanlim <kimsanlim27@gmail.com>
2023-06-21 15:10:07 +0700

    Seed job data to realm

--- app/models/Job.js
+++ app/models/Job.js
@@ -1,16 +1,50 @@
-import listJobs from '../data/json/list_jobs';
-import { containsAny } from '../utils/math';
+// import listJobs from '../data/json/list_jobs';
+// import { containsAny } from '../utils/math';
+
+// export default class Job {
+//   static findByCode = (code) => {
+//     return listJobs.filter(x => x.code == code)[0];
+//   }
+
+//   static findAllByCodes = (codes = []) => {
+//     return listJobs.filter(job => codes.includes(job.code))
+//   }
+
+//   static findAllByPersonalityTypes = (types = []) => {
+//     return listJobs.filter(job => containsAny(job.personality_type + '', types))
+//   }
+// }
+
+
+import BaseModel from './BaseModel'
+import uuidv4 from '../utils/uuidv4';
+import jobs from '../data/json/jobs.json';
+
+const MODEL = 'Job'
 
 export default class Job {
-  static findByCode = (code) => {
-    return listJobs.filter(x => x.code == code)[0];
+  static seedData = () => {
+    jobs.map((job) => {
+      BaseModel.create(MODEL, this.getFormattedData(job), 'modified')
+    })
   }
 
-  static findAllByCodes = (codes = []) => {
-    return listJobs.filter(job => codes.includes(job.code))
+  static getAll = () => {
+    return BaseModel.getAll(MODEL)
   }
 
-  static findAllByPersonalityTypes = (types = []) => {
-    return listJobs.filter(job => containsAny(job.personality_type + '', types))
+  // private method
+  static getFomattedVideos = (videos) => {
+    let result = []
+    videos.map(video => {
+      result.push(video.id)
+    })
+    return result
+  }
+
+  static getFormattedData(school) {
+    let {logo, videos, name_km, name_en, ...data} = school
+    // data = {...data, uuid: uuidv4(), logo: school.logo.url, videos: this.getFomattedVideos(school.videos)}
+    return {...data, uuid: uuidv4(), logo: school.logo.url, videos: this.getFomattedVideos(school.videos)}
   }
 }
--- app/services/seed_data_service.js
+++ app/services/seed_data_service.js
@@ -1,7 +1,9 @@
 import School from '../models/School'
 import CollegeMajor from '../models/CollegeMajor'
+import Job from '../models/Job'
 
 export const seedDataToRealm = () => {
   School.getAll().length == 0 && School.seedData()
   CollegeMajor.getAll().length == 0 && CollegeMajor.seedData()
+  Job.getAll().length == 0 && Job.seedData()
 }
