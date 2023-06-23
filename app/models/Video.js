fdaf133a1639a3f2f37237311831c34101a68ec4
Kimsanlim <kimsanlim27@gmail.com>
2023-06-23 10:02:04 +0700

    Seed video data to realm

--- app/models/Video.js
+++ app/models/Video.js
@@ -1,18 +1,23 @@
 import BaseModel from './BaseModel'
 import uuidv4 from '../utils/uuidv4';
+import videos from '../data/json/videos.json';
 
 const MODEL = 'Video'
 
 export default class Video {
-  static seedData = (videos) => {
+  static seedData = () => {
     videos.map(video => {
       if (!this.findById(video.id))
         this.create(video)
     })
   }
 
+  static getAll = () => {
+    return BaseModel.getAll()
+  }
+
   static create = (data) => {
-    BaseModel.create(MODEL, {...data, uuid: uuidv4()}, 'modified')
+    BaseModel.create(MODEL, {...data, uuid: uuidv4()})
   }
 
   static findById = (id) => {
