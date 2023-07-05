9f89b000afce5e7b1a9b16a5559523eb2db95085
Kimsanlim <kimsanlim27@gmail.com>
2023-07-05 13:41:37 +0700

    Include other_occupation when send API request to upload the user

--- app/api/user.js
+++ app/api/user.js
@@ -20,6 +20,7 @@ export const uploadUser = async(userUuid) => {
     high_school_code: user.highSchoolCode,
     middle_school_id: user.middleSchoolId,
     registered_at: user.createdAt,
+    other_occupation: user.other_occupation,
     device_id: await getUniqueId(),
     device_type: isTablet() ? "tablet" : "mobile",
     device_os: Platform.OS,
