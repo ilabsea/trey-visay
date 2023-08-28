7a102350d9ed17f9c52d89da27209f2f21d1aba6
Kimsanlim <kimsanlim27@gmail.com>
2023-08-28 17:32:16 +0700

    Fix issue of screen get reloaded when select/unselect an item in the college major and job multi select screen

--- app/screens/JobSelection/JobSelectMultipleScreen.js
+++ app/screens/JobSelection/JobSelectMultipleScreen.js
@@ -1,7 +1,5 @@
 import React, {useState} from 'react'
-import { View, TouchableWithoutFeedback, Keyboard } from 'react-native'
-import { Text } from '../../components';
-import { Divider } from 'react-native-paper';
+import { View } from 'react-native'
 import Quiz from '../../models/Quiz';
 import { useDispatch } from 'react-redux';
 import { setCurrentQuiz } from '../../redux/features/quiz/quizSlice';
@@ -12,7 +10,6 @@ import Job from '../../models/Job';
 
 const JobSelectMultipleScreen = ({route, navigation}) => {
   const [textSearch, setTextSearch] = useState('');
-  const [selectedItem, setSelectedItem] = useState(0);
   const [errorMsg, setErrorMsg] = useState('')
   const currentQuiz = Quiz.findByUuid(route.params.quizUuid);
   const dispatch = useDispatch();
@@ -20,8 +17,8 @@ const JobSelectMultipleScreen = ({route, navigation}) => {
   const data = Job.findAllByPersonalityTypes(currentQuiz.topPersonalityTypes).map(x => ({ name: x.name, value: x.code }))
 
   const handleSubmit = () => {
-    if (selectedItem == 0 || selectedItem > 3)
-      return setErrorMsg(selectedItem == 0 ? "សូមជ្រើសរើសយ៉ាងតិច 1" : "សូមជ្រើសរើសយ៉ាងច្រើន 3")
+    if (formRef.current?.getSelectedValues().length == 0 || formRef.current?.getSelectedValues().length > 3)
+      return setErrorMsg(formRef.current?.getSelectedValues().length == 0 ? "សូមជ្រើសរើសយ៉ាងតិច 1" : "សូមជ្រើសរើសយ៉ាងច្រើន 3")
 
     Quiz.write(()=> {
       currentQuiz.jobCodeSelections = formRef.current?.getSelectedValues();
@@ -30,32 +27,25 @@ const JobSelectMultipleScreen = ({route, navigation}) => {
     navigation.navigate('JobSelectOneScreen', {quizUuid: currentQuiz.uuid});
   }
 
+  const onSetTextSearch = (text) => {
+    setTextSearch(text);
+    !text && formRef.current?.resetListIndices();
+  }
+
   return (
     <React.Fragment>
       <SearchableHeader title="ជម្រើសអាជីពការងារ" placeholder="ស្វែងរកអាជីពការងារ"
-        searchedText={textSearch} setSearchedText={(text) => setTextSearch(text)}
+        searchedText={textSearch} setSearchedText={(text) => onSetTextSearch(text)}
       />
       <View style={{margin: 16, flex: 1, backgroundColor: '#fff', borderRadius: 4, overflow: 'hidden'}}>
-        <TouchableWithoutFeedback onPress={() => Keyboard.dismiss()}>
-          <View style={{padding: 16}}>
-            <Text>ចូរជ្រើសរើសអាជីពការងារដែលអ្នកពេញចិត្តក្នុងការបន្តការសិក្សានាពេលអនាគត យ៉ាងច្រើនបំផុត៣មុខជំនាញ៖</Text>
-            <Text>{selectedItem}/3</Text>
-          </View>
-        </TouchableWithoutFeedback>
-
-        <Divider />
-
-        <View style={{flex: 1}}>
-          <CheckboxGroup
-            ref={formRef}
-            name={"jobs"}
-            options={data.filter(job => job.name.includes(textSearch))}
-            textSearch={textSearch}
-            errorMessage={errorMsg}
-            updateSelectedItem={(items) => setSelectedItem(items)}
-            type='job'
-          />
-        </View>
+        <CheckboxGroup
+          ref={formRef}
+          options={data.filter(job => job.name.includes(textSearch))}
+          textSearch={textSearch}
+          errorMessage={errorMsg}
+          type='job'
+          headerLabel={`ចូរជ្រើសរើសអាជីពការងារដែលអ្នកពេញចិត្តក្នុងការបន្តការសិក្សានាពេលអនាគត យ៉ាងច្រើនបំផុត 3មុខជំនាញក្នុងចំណោម ${data.length}មុខជំនាញ៖ `}
+        />
       </View>
 
       <FooterBar icon='keyboard-arrow-right' text='បន្តទៀត' onPress={handleSubmit} />
--- app/screens/MajorSelection/MajorSelectMultipleScreen.js
+++ app/screens/MajorSelection/MajorSelectMultipleScreen.js
@@ -1,7 +1,5 @@
 import React, { useState } from 'react'
-import { View, Keyboard, TouchableWithoutFeedback } from 'react-native'
-import { Text } from '../../components';
-import { Divider } from 'react-native-paper';
+import { View } from 'react-native'
 import Quiz from '../../models/Quiz';
 import { useSelector, useDispatch } from 'react-redux';
 import { setCurrentQuiz } from '../../redux/features/quiz/quizSlice';
@@ -12,7 +10,6 @@ import CollegeMajor from '../../models/CollegeMajor';
 
 const MajorSelectMultipleScreen = ({route, navigation}) => {
   const [textSearch, setTextSearch] = useState('');
-  const [selectedItem, setSelectedItem] = useState(0);
   const [errorMsg, setErrorMsg] = useState('')
   const currentQuiz = Quiz.findByUuid(route.params.quizUuid);
   const dispatch = useDispatch();
@@ -21,8 +18,8 @@ const MajorSelectMultipleScreen = ({route, navigation}) => {
   let formRef = React.useRef()
 
   const handleSubmit = () => {
-    if (selectedItem == 0 || selectedItem > 3)
-      return setErrorMsg(selectedItem == 0 ? "សូមជ្រើសរើសយ៉ាងតិច 1" : "សូមជ្រើសរើសយ៉ាងច្រើន 3")
+    if (formRef.current?.getSelectedValues().length == 0 || formRef.current?.getSelectedValues().length > 3)
+      return setErrorMsg(formRef.current?.getSelectedValues().length == 0 ? "សូមជ្រើសរើសយ៉ាងតិច 1" : "សូមជ្រើសរើសយ៉ាងច្រើន 3")
 
     Quiz.write(()=> {
       currentQuiz.majorCodeSelections = formRef.current?.getSelectedValues();
@@ -31,32 +28,25 @@ const MajorSelectMultipleScreen = ({route, navigation}) => {
     navigation.navigate('MajorSelectOneScreen', {quizUuid: currentQuiz.uuid});
   }
 
+  const onSetTextSearch = (text) => {
+    setTextSearch(text);
+    !text && formRef.current?.resetListIndices();
+  }
+
   return (
     <React.Fragment>
       <SearchableHeader title="ជម្រើសជំនាញកម្រិតឧត្តមសិក្សា" placeholder="ស្វែងរកជំនាញកម្រិតឧត្តមសិក្សា"
-        searchedText={textSearch} setSearchedText={(text) => setTextSearch(text)}
+        searchedText={textSearch} setSearchedText={(text) => onSetTextSearch(text)}
       />
       <View style={{margin: 16, flex: 1, backgroundColor: '#fff', borderRadius: 4, overflow: 'hidden'}}>
-        <TouchableWithoutFeedback onPress={() => Keyboard.dismiss()}>
-          <View style={{padding: 16}}>
-            <Text>ចូរជ្រើសរើសមុខជំនាញដែលអ្នកពេញចិត្តក្នុងការបន្តការសិក្សានាពេលអនាគតយ៉ាងច្រើនបំផុត ៣មុខជំនាញ៖ </Text>
-            <Text>{selectedItem}/3</Text>
-          </View>
-        </TouchableWithoutFeedback>
-
-        <Divider />
-
-        <View style={{flex: 1}}>
-          <CheckboxGroup
-            ref={formRef}
-            name={"majors"}
-            options={data.filter(major => major.name.includes(textSearch))}
-            textSearch={textSearch}
-            errorMessage={errorMsg}
-            updateSelectedItem={(items) => setSelectedItem(items)}
-            type='major'
-          />
-        </View>
+        <CheckboxGroup
+          ref={formRef}
+          options={data.filter(major => major.name.includes(textSearch))}
+          textSearch={textSearch}
+          errorMessage={errorMsg}
+          type='major'
+          headerLabel={`ចូរជ្រើសរើសមុខជំនាញដែលអ្នកពេញចិត្តក្នុងការបន្តការសិក្សានាពេលអនាគតយ៉ាងច្រើនបំផុត 3មុខជំនាញក្នុងចំណោម ${data.length}មុខជំនាញ៖ `}
+        />
       </View>
 
       <FooterBar icon='keyboard-arrow-right' text='បន្តទៀត' onPress={handleSubmit} />
