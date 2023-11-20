85a453c22de93c5ca600148fd53c13ff892da3b1
Kimsanlim <kimsanlim27@gmail.com>
2023-11-20 13:27:15 +0700

    Update the user's UUID to random 12 characters

--- app/utils/id_util.js
+++ app/utils/id_util.js
@@ -1,5 +1,5 @@
 export default function randomId() {
-  return 'xxxxxxxx'.replace(/x/g, function (c) {
+  return 'xxxxxxxxxxxx'.replace(/x/g, function (c) {
     var r = Math.random() * 16 | 0;
     return r.toString(16);
   });
